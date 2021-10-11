18f5c092afc4eb74c18e59b63aef3eb8342b7c85
Mirzamehdi <mirzemehdi@gmail.com>
2021-10-11 19:42:37 +0200

    Adding MainCoroutineRule for not getting fleaky unit test result

--- domain/src/test/java/com/mmk/domain/repository/BaseRepositoryTest.kt
+++ domain/src/test/java/com/mmk/domain/repository/BaseRepositoryTest.kt
@@ -2,26 +2,32 @@ package com.mmk.domain.repository
 
 import com.google.common.truth.Truth.assertThat
 import com.mmk.domain.model.Result
+import com.mmk.domain.util.MainCoroutineRule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import java.lang.IllegalArgumentException
 
 @ExperimentalCoroutinesApi
 class BaseRepositoryTest {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     private lateinit var baseRepository: BaseRepository
 
     @Before
     fun setUp() {
-        baseRepository = object : BaseRepository(Dispatchers.Unconfined) {}
+        baseRepository = object : BaseRepository(mainCoroutineRule.dispatcher) {}
     }
 
 
     @Test
-    fun `base executeInBackground returns Success when response is success`() = runBlockingTest {
+    fun `base executeInBackground returns Success when response is success`() = mainCoroutineRule.runBlockingTest {
         //Given
         val func: () -> Result<String> = { Result.Success("") }
         //When
@@ -33,7 +39,7 @@ class BaseRepositoryTest {
     }
 
     @Test
-    fun `base executeInBackground returns Fail when response is failed`() = runBlockingTest {
+    fun `base executeInBackground returns Fail when response is failed`() = mainCoroutineRule.runBlockingTest {
         //Given
         val func: () -> Result<String> = { Result.Error("Error") }
         //When
@@ -48,7 +54,7 @@ class BaseRepositoryTest {
     //
     @Test
     fun `base executeInBackground returns Fail when function throws exception`() =
-        runBlockingTest {
+        mainCoroutineRule.runBlockingTest {
             ///Given
             val errorMessage="Error occurred"
             val func: () -> Result<String> = { throw IllegalArgumentException(errorMessage) }
