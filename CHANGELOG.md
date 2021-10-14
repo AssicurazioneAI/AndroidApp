7b679a4f309dc8aa6beb90941b2fa41155f28bdc
Mirzamehdi <mirzemehdi@gmail.com>
2021-10-14 05:24:53 +0200

    Updating github action files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.0.2
+## 1.0.3
 
 - Implementing CI/CD to the project
 - Integrating Firebase in order to send a mail automatically when there is a new release
--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -4,7 +4,7 @@ object ConfigData {
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
     const val minSdkVersion = 21
-    const val versionCode = 3
-    const val versionName = "1.0.2"
+    const val versionCode = 4
+    const val versionName = "1.0.3"
 
 }
