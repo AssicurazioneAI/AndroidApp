3998d6269fbaf8cf78f17ca8be0c1372ab1872d4
Mirzamehdi <mirzemehdi@gmail.com>
2021-11-07 04:20:25 +0100

    New version with camera functionality

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -4,7 +4,7 @@ object ConfigData {
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
     const val minSdkVersion = 21
-    const val versionCode = 4
-    const val versionName = "1.0.3"
+    const val versionCode = 5
+    const val versionName = "1.0.6"
 
 }
