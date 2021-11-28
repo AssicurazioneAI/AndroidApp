8988775d993bc5300e2f818f191e6a7b175f4d75
Mirzamehdi <mirzemehdi@gmail.com>
2021-11-29 00:41:02 +0100

    Testing CarAnalyzeViewModel and fixing bottomsheet expanded bug

--- app/src/main/java/com/mmk/assicurazioneai/ui/features/cardamage/analyze/CarDamageAnalyzeFragment.kt
+++ app/src/main/java/com/mmk/assicurazioneai/ui/features/cardamage/analyze/CarDamageAnalyzeFragment.kt
@@ -1,18 +1,20 @@
 package com.mmk.assicurazioneai.ui.features.cardamage.analyze
 
 import android.graphics.RectF
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.bundleOf
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mmk.assicurazioneai.R
 import com.mmk.assicurazioneai.databinding.FragmentAnalyzeDamageBinding
-import com.mmk.assicurazioneai.databinding.FragmentCameraBinding
 import com.mmk.assicurazioneai.ui.base.BaseFragment
 import com.mmk.assicurazioneai.ui.base.BaseViewModel
 import com.mmk.assicurazioneai.ui.features.cardamage.damageresult.DamageResultDialogFragment
@@ -21,6 +23,11 @@ import com.mmk.assicurazioneai.utils.observeSingleEvent
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.google.android.material.shape.MaterialShapeDrawable
+
+import com.google.android.material.shape.ShapeAppearanceModel
+import timber.log.Timber
+
 
 class CarDamageAnalyzeFragment : BaseFragment(R.layout.fragment_analyze_damage) {
     override val binding: FragmentAnalyzeDamageBinding by viewBinding(FragmentAnalyzeDamageBinding::inflate)
@@ -37,10 +44,13 @@ class CarDamageAnalyzeFragment : BaseFragment(R.layout.fragment_analyze_damage) 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
         binding.executePendingBindings()
-        changeBottomSheetState(binding.bottomSheet.root,BottomSheetBehavior.STATE_COLLAPSED)
+        changeBottomSheetState(binding.bottomSheet.root, BottomSheetBehavior.STATE_COLLAPSED)
         lifecycleScope.launch {
             delay(200)
-            changeBottomSheetState(binding.bottomSheet.root,BottomSheetBehavior.STATE_HALF_EXPANDED)
+            changeBottomSheetState(
+                binding.bottomSheet.root,
+                BottomSheetBehavior.STATE_EXPANDED
+            )
         }
     }
 
@@ -51,7 +61,18 @@ class CarDamageAnalyzeFragment : BaseFragment(R.layout.fragment_analyze_damage) 
         }
 
         viewModel.onImageSent.observeSingleEvent(viewLifecycleOwner) {
-            val coordinates = listOf(RectF(100f, 200f, 500f, 600f))
+            val imageRectF = getImageBounds(binding.imageView)
+
+            Timber.e(imageRectF.toString())
+
+            val coordinates = listOf(
+                RectF(
+                    imageRectF.centerX() - 100,
+                    imageRectF.centerY() - 100,
+                    imageRectF.centerX() + 100,
+                    imageRectF.centerY() + 100
+                )
+            )
             drawDamageRectangle(coordinates)
             lifecycleScope.launch {
                 val damageType = "HARD DAMAGE"
@@ -68,7 +89,7 @@ class CarDamageAnalyzeFragment : BaseFragment(R.layout.fragment_analyze_damage) 
         super.setClicks()
         binding.bottomSheet.sendButton.setOnClickListener {
             viewModel.sendImage()
-            changeBottomSheetState(binding.bottomSheet.root,BottomSheetBehavior.STATE_COLLAPSED)
+            changeBottomSheetState(binding.bottomSheet.root, BottomSheetBehavior.STATE_COLLAPSED)
 
         }
         binding.bottomSheet.retakeButton.setOnClickListener {
@@ -81,8 +102,51 @@ class CarDamageAnalyzeFragment : BaseFragment(R.layout.fragment_analyze_damage) 
     }
 
     private fun changeBottomSheetState(view: View, state: Int) {
-        BottomSheetBehavior.from(view).state = state
+        val bottomSheetBehavior = BottomSheetBehavior.from(view)
+        bottomSheetBehavior.isDraggable = false
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    ViewCompat.setBackground(view, createMaterialShapeDrawable(view))
+                }
+            }
 
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+        bottomSheetBehavior.state = state
+
+    }
+
+
+    private fun createMaterialShapeDrawable(bottomSheet: View): MaterialShapeDrawable {
+        val shapeAppearanceModel =
+            //Create a ShapeAppearanceModel with the same shapeAppearanceOverlay used in the style
+            ShapeAppearanceModel.builder(context, 0, R.style.CustomShapeAppearanceBottomSheetDialog)
+                .build()
+
+        //Create a new MaterialShapeDrawable (you can't use the original MaterialShapeDrawable in the BottomSheet)
+        val currentMaterialShapeDrawable = bottomSheet.background as MaterialShapeDrawable
+        val newMaterialShapeDrawable = MaterialShapeDrawable(shapeAppearanceModel)
+        //Copy the attributes in the new MaterialShapeDrawable
+        newMaterialShapeDrawable.initializeElevationOverlay(context)
+        newMaterialShapeDrawable.fillColor = currentMaterialShapeDrawable.fillColor
+        newMaterialShapeDrawable.tintList = currentMaterialShapeDrawable.tintList
+        newMaterialShapeDrawable.elevation = currentMaterialShapeDrawable.elevation
+        newMaterialShapeDrawable.strokeWidth = currentMaterialShapeDrawable.strokeWidth
+        newMaterialShapeDrawable.strokeColor = currentMaterialShapeDrawable.strokeColor
+        newMaterialShapeDrawable.strokeColor = currentMaterialShapeDrawable.strokeColor
+        return newMaterialShapeDrawable
+    }
+
+    private fun getImageBounds(imageView: ImageView): RectF {
+        val bounds = RectF()
+        if (imageView.drawable != null) {
+            imageView.imageMatrix.mapRect(bounds, RectF(imageView.drawable.bounds))
+        }
+        return bounds
     }
 
 
